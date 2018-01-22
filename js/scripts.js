af7e1656e3f1cd0376fdbb266abdfb889b28fc44
Masen Matthews <masenmatthews@gmail.com>
2018-01-22 10:55:55 -0800

    initialize git

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
-$(document.ready(function();
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
+
+$(document).ready(function() {
+  $("form#newContact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#newFirstName").val();
+    var inputtedLastName = $("input#newLastName").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li>span class='first-name'>" + newContact.newFirstName + "</span></li>");
+    $("ul#contacts").append("<li>span class='last-name'>" + newContact.newLastName + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#displayContact").show();
+      $("#displayContact h3").text(newContact.firstName);
+      $("#newFirstName").text(newContact.firstName);
+      $("#newLastName").text(newContact.lastName);
+    });
+
+    $("input#newFirstName").val("");
+    $("input#newLastName").val("");
+  });
+});
